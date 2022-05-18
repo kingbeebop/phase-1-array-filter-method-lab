eecaf8b06ebed655d4948c89e5e8dfd5d86581e2
Brendan Ward <kingbeebop@gmail.com>
2022-05-18 09:22:39 -0400

    commit tree eb194a8095e211393a3fde83cdf08f69c53c7e22

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const matching = drivers.filter(driver => driver.toLowerCase() == name.toLowerCase())
+    return matching
+}
+
+function fuzzyMatch(drivers, string){
+    const matching = drivers.filter(driver => driver.substring(0,string.length) === string)
+    return matching
+}
+
+function matchName(drivers, string){
+    const matching = drivers.filter(driver => driver.name == string)
+    return matching
+}
